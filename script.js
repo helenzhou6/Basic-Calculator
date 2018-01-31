afb771c013265b41f3b1c82e41c70feaf74c8ef9
Helen Zhou <helen.zhou6@gmail.com>
2018-01-31 22:51:41 +0000

    Link javascript with front end.

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 window.onload=function(){
-	var display = document.getElementById('display--js');
-	var answer = document.getElementById('answer--js');
+	var display = document.getElementById('display--js').firstChild;
+	var answer = document.getElementById('answer--js').firstChild;
 	var input = document.getElementById('cells--js');
 
 	var resultArray = '0';
 	var opIsPressed = false;
 	var decIsPressed = false;
 
+var reset = function(){
+	display.nodeValue = '0';
+	answer.nodeValue = '0';
+
+	resultArray = '0';
+	opIsPressed = false;
+	decIsPressed = false;
+
+}
+	reset();
+
+	var rgxr = function(string){
+		return string.replace(/(?!\d)[.](?=[-+/*])|^0+(?=\d)|(?<=[-+/*])([0]+)(?=\d)|(?<=\d+.\d+)[0]+(?=[-+/*])/g, '').replace(/(?<=[-+/*])[.]/g, '0.');
+	}
 	var clickFunc = function(e) {
 		var num = e.target.dataset.num;
 		var op = e.target.dataset.ops;
@@ -26,6 +40,7 @@ window.onload=function(){
 			}
 
 			resultArray += num;
+			resultArray = rgxr(resultArray);
 			opIsPressed = false;
 		// If an operator type is pressed
 		} else if (op) {
@@ -35,27 +50,21 @@ window.onload=function(){
 				resultArray = resultArray.slice(0, -1);
 			}
 			resultArray += op;
+			resultArray = rgxr(resultArray);
 			decIsPressed = false;
 			opIsPressed = true;
 
 		} else if (func) {
 				if ((func === '=') && (!opIsPressed)) {
-					console.log(eval(resultArray));
+					answer.nodeValue = (eval(resultArray));
+				} else if (func === 'ac') {
+					reset();
+				} else if (func === 'ce') {
+
 				}
 		}
-
-		console.log(resultArray);
-
-		// var text = document.createTextNode(resultArray);
-		// display.appendChild(text);
+		display.nodeValue = resultArray;
 	}
 	input.addEventListener('click', clickFunc, false);
-
-	var string = '000.2+.342+32432.+000200+0.0002+9.9+000.9+.9';
-	// JUST NEED TO DO BEGINNING OF STRING MULTI ZEROS
-	console.log(string.replace(/(?!\d)[.](?=[+-/*])/g, '').replace(/(?<=[-+/*])[.]/g, '0.').replace(/(?<=[-+/*])([0]+)(?=\d)/g, '').replace(/^0+(?=\d)/g, ''));
-	// data-func = ac and ce and equals
-	// data-num = .0123456789
-	// data-ops =  + / - %
 
 }
